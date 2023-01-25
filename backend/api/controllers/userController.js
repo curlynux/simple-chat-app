4fac392edeca2d0f97dab5a70cea9265de158c40
curlynux <curlynux@gmail.com>
2023-01-25 09:42:41 +0000

    add user admin in db

--- backend/api/controllers/userController.js
+++ backend/api/controllers/userController.js
@@ -1,17 +1,30 @@
 const User = require("../models/userModel");
-const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
 exports.signup = async (req, res, next) => 
 {
-
     console.log(req.body);
+    
     const user = new User({
-        login: req.body.login, 
-        email: req.body.email, 
-        password: req.body.password, 
-        role: req.body.role
-    });
+        login: req.body.login,
+        email: req.body.email,
+        password: req.body.password,
+        role: "client"
+    })
+
+    const userModel = mongoose.model("User");
+    const query = {login: "admin"}
+    const update = {
+        $setOnInsert: {
+            login: "admin",
+            email: "admin@simplechatapp.fr",
+            password: "admin",
+            role: "admin"
+        }
+    }
+    const options = {upsert: true}
+    userModel.findOneAndUpdate(query, update, options)
+    .catch(error => console.error(error))
     await user.save().then(() => res.status(201).json({message: "User created successfully !"}))
     console.log("user saved successfully !");
 }
