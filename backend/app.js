b08de7c8d12428c150fbb30c63e1aeb40c34d210
curlynux <curlynux@gmail.com>
2023-07-30 17:12:55 +0000

    push change

--- backend/app.js
+++ backend/app.js
@@ -5,30 +5,38 @@ const app = express();
 const morgan = require("morgan");
 require("dotenv").config();
 const userRoutes = require("./api/routes/userRoutes");
-const messageRoutes = require("./api/routes/messagesRoutes")
+const messageRoutes = require("./api/routes/messagesRoutes");
 
 mongoose.set("strictQuery", true);
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(userRoutes)
-app.use(messageRoutes)
-app.use((req, res, next) =>
-    {
-        res.setHeader("Access-Control-Allow-Origin", "*")
-        res.setHeader("Access-Control-Allow-Headers", "Origin, X-requested-With, Content, Accept, Content-Type, Authorization");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-        next();
-    });
+app.use(userRoutes);
+app.use(messageRoutes);
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-requested-With, Content, Accept, Content-Type, Authorization"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	);
+	next();
+});
 
-mongoose.connect(`mongodb+srv://${process.env.MONGOLOGIN}:${process.env.MONGOPASSWORD}@simple-chat-app.gzhk7e9.mongodb.net/?retryWrites=true&W=majority`,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-.then(() => console.log("connected successfully to MongoDB Atlas !"))
-.catch(() => console.log("connexion to mongodb failed !"));
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.MONGOLOGIN}:${process.env.MONGOPASSWORD}@simple-chat-app.gtajhuy.mongodb.net/?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then(() => console.log("connected successfully to MongoDB Atlas !"))
+	.catch(() => console.log("connexion to mongodb failed !"));
 
 module.exports = app;
